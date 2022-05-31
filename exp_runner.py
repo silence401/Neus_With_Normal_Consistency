aac91382c3e1b2e216ad6c49282139bf9c2f69d4
silence401 <913725480@qq.com>
2022-05-31 19:04:14 +0800

    save camera

--- exp_runner.py
+++ exp_runner.py
@@ -52,7 +52,7 @@ class Runner:
         # Weights
         self.igr_weight = self.conf.get_float('train.igr_weight')
         self.mask_weight = self.conf.get_float('train.mask_weight')
-        self.norm_consist_weight = self.conf.get_float('train.norm_consist_weight')
+       # self.norm_consist_weight = self.conf.get_float('train.norm_consist_weight')
         self.is_continue = is_continue
         self.mode = mode
         self.model_list = []
@@ -143,7 +143,7 @@ class Runner:
             loss = color_fine_loss +\
                    eikonal_loss * self.igr_weight +\
                    mask_loss * self.mask_weight +\
-                   norm_consistency_error * self.norm_consist_weight
+                   norm_consistency_error * self.mask_weight
 
             self.optimizer.zero_grad()
             loss.backward()
